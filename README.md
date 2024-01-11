7b6e2a8c4d0441bed425acc146f3daba3c3584a2
nanashi10211 <chakma.kalin10211@gmail.com>
2024-01-11 07:25:09 +0600

    project reshape

---
+++ README.md
@@ -0,0 +1,7 @@
+<!-- @format -->
+
+# Simple E-commerce market
+
+- This project done using `express.js` framework
+- In this project i implemented login logout system
+- User can add product
--- app.js
+++ app.js
@@ -1,68 +1,67 @@
+/** @format */
+
 const express = require('express');
 const path = require('path');
 
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
 // database
 const db = require('./utils/database');
 
-// session 
+// session
 const session = require('express-session');
 const sessionStore = require('connect-session-sequelize')(session.Store);
 
 // flash session
 const flash = require('connect-flash');
 
-// csrf token 
+// csrf token
 const csrf = require('csurf');
 
 // send mail using nodemailer and gmail
- const nodemailer = require('nodemailer');
-
-
-
-
+const nodemailer = require('nodemailer');
 
 const storeDb = new sessionStore({
-    db: db
+	db: db,
 });
 
-// router 
-const adminRouter = require("./routes/adminRouter");
-const shopRouter = require("./routes/shopRouter");
+// router
+const adminRouter = require('./routes/adminRouter');
+const shopRouter = require('./routes/shopRouter');
 const authRouter = require('./routes/auth');
 
-// model 
-const Product = require("./model/productModel");
-const User = require("./model/users");
-const Cart = require("./model/cart");
-const CartItem = require("./model/cart-item");
+// model
+const Product = require('./model/productModel');
+const User = require('./model/users');
+const Cart = require('./model/cart');
+const CartItem = require('./model/cart-item');
 
 // test model
-const Person = require("./model/test/person");
-const Contact = require("./model/test/contactInfo");
+const Person = require('./model/test/person');
+const Contact = require('./model/test/contactInfo');
 
 const app = express();
-
 
 // csrf protection
 const csrfProtection = csrf();
 
 // setting view engine
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 // for parsing data
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 // setting static file dir
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 // use session
-app.use(session({ 
-    secret: 'long string in session',
-    resave: false, 
-    saveUninitialized: false,
-    store: storeDb
-    }));
+app.use(
+	session({
+		secret: 'long string in session',
+		resave: false,
+		saveUninitialized: false,
+		store: storeDb,
+	}),
+);
 
 // csrf protection
 app.use(csrfProtection);
@@ -72,41 +71,38 @@ app.use(flash());
  * setting routes
  */
 app.use((req, res, next) => {
-    if(!req.session.user) {
-        return next();
-    }
-    User.findOne({where: {id: req.session.user.id}})
-    .then(user => {
-        req.user = user;
-        next();
-    })
-    .catch(err => {
-        console.log(err);
-    });
+	if (!req.session.user) {
+		return next();
+	}
+	User.findOne({ where: { id: req.session.user.id } })
+		.then((user) => {
+			req.user = user;
+			next();
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 // available for all render
 app.use((req, res, next) => {
-    res.locals.isLoggedIn = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
+	res.locals.isLoggedIn = req.session.isLoggedIn;
+	res.locals.csrfToken = req.csrfToken();
+	next();
 });
 
-app.use("/admin", adminRouter);
+app.use('/admin', adminRouter);
 
 app.use(shopRouter);
 
 app.use(authRouter);
-
-
-
 
 // creating relation
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 User.hasOne(Cart);
 Cart.belongsTo(User);
-Cart.belongsToMany(Product, {through: CartItem});
-Product.belongsToMany(Cart, {through: CartItem});
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 /**
  * test association
  */
@@ -117,10 +113,10 @@ Product.belongsToMany(Cart, {through: CartItem});
  */
 
 Person.hasOne(Contact, {
-    foriegnKey: {
-        type: Sequelize.UUID,
-        allowNull: false
-    }
+	foriegnKey: {
+		type: Sequelize.UUID,
+		allowNull: false,
+	},
 });
 Contact.belongsTo(Person);
 
@@ -130,12 +126,9 @@ Contact.belongsTo(Person);
 storeDb.sync();
 // .sync({force: true})
 db.sync()
-.then(result => {
-    app.listen(9000);
-})
-.catch(err => {
-    console.log(err);
-});
-
-
-
+	.then((result) => {
+		app.listen(9000);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
